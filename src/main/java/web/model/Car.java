02414f6c29598d6dc3172a22b73aa078b6bd5c77
Top-Cop <likhanov.al@ya.ru>
2024-02-26 11:03:17 +0800

    add Car class

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,27 @@
+package web.model;
+
+public class Car {
+    private String model;
+    private int series;
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
